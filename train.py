d492c66760d8a24cadaf1a76b8a5d8c31b30e2d5
Moses7D <Moses7D@users.noreply.github.com>
2020-01-05 15:25:12 +0200

    Update train.py

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ from yolo3.utils import get_random_data
 def _main():
     annotation_path = 'GreekCuisineData/train.txt'
     log_dir = 'GreekCuisineData/logs/000/'
-    classes_path = 'GreekCuisineData/weights/voc_classes.txt'
-    anchors_path = 'GreekCuisineData/weights/yolo_anchors.txt'
+    classes_path = 'GreekCuisineData/gcp_classes.txt'
+    anchors_path = 'GreekCuisineData/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
@@ -27,10 +27,10 @@ def _main():
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+            freeze_body=2, weights_path='GreekCuisineData/weights/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='GreekCuisineData/weights/yolo_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
